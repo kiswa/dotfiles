a1b5293240bd06ed3358e09dcbae9c0f2f27d826
Matthew Ross <kiswa@users.noreply.github.com>
2020-07-22 13:50:19 -0400

    Previous station is gone.

--- bin/weather.py
+++ bin/weather.py
@@ -2,10 +2,10 @@
 
 import urllib.request, json
 
-jsonurl = "https://stationdata.wunderground.com/cgi-bin/stationlookup?station=KGAALPHA174&units=english&v=2.0&format=json"
+jsonurl = "https://stationdata.wunderground.com/cgi-bin/stationlookup?station=KGAALPHA136&units=english&v=2.0&format=json"
 
 with urllib.request.urlopen(jsonurl) as url:
     data = json.loads(url.read().decode())
-    current = data['stations']['KGAALPHA174']
+    current = data['stations']['KGAALPHA136']
     print(str(current['temperature']) + '°F' + ' - ' + str(current['wind_speed']) + 'mph')
 
