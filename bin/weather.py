271180fcf0992bece133fe580ef095adeb91d376
Matthew Ross <kiswa.com@gmail.com>
2022-04-14 17:49:36 -0500

    Add colorscheme file and remove old key

--- bin/weather.py
+++ bin/weather.py
@@ -2,7 +2,7 @@
 
 import urllib.request, json
 
-jsonurl = "https://api.openweathermap.org/data/2.5/weather?q=Flower+Mound&units=imperial&appid=9d0f8ec6464502789eb644857f2cebe4"
+jsonurl = "https://api.openweathermap.org/data/2.5/weather?q=Flower+Mound&units=imperial&appid="
 
 with urllib.request.urlopen(jsonurl) as url:
     data = json.loads(url.read().decode())
