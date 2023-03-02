ff9a86880bfe9d50fe4ec64eb5e4577725e4ee2b
Matt Ross <kiswa.com@gmail.com>
2023-03-02 14:35:02 -0600

    Add pacman hook

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ mkdir -p ~/.vim/sessions
 
 cp .bashrc .conkyrc .vimrc .gitconfig .npmrc .tmux.conf ~/
 cp -R bin ~/
+
+# Arch Linux pacman hook to only keep the latest two versions of a package
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+sudo -i cp $SCRIPTPATH/20-remove-cache-packages.hook /etc/pacman.d/hooks
