18f1fbac23a7ccd0f0ce64085e06bb65242946ee
Matthew Ross <kiswa.com@gmail.com>
2023-11-22 23:20:26 -0600

    Ensure directory exists

--- install.sh
+++ install.sh
@@ -8,4 +8,5 @@ cp -R bin ~/
 
 # Arch Linux pacman hook to only keep the latest two versions of a package
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-sudo -i cp $SCRIPTPATH/20-remove-cache-packages.hook /etc/pacman.d/hooks
+sudo -i mkdir /etc/pacman.d/hooks
+sudo -i cp $SCRIPTPATH/20-remove-cache-packages.hook /etc/pacman.d/hooks/
